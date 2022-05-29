cf8806b8aefdebf528953ab137c9decdfb32b91f
Keith Lam <keithlam0110@gmail.com>
2022-05-29 22:37:31 +0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:snipdaily/MainClass.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( const Login());
 }
 
