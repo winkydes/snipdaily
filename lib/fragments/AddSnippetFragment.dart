baef28b322ddea3a42c89e35e2554aac4c4d7476
winkydes <keithlam0110@gmail.com>
2022-06-01 18:11:40 +0800

    add snippet fragment code fix

--- lib/fragments/AddSnippetFragment.dart
+++ lib/fragments/AddSnippetFragment.dart
@@ -8,39 +8,29 @@ class AddSnippetFragment extends StatefulWidget {
 }
 
 class _AddSnippetFragmentState extends State<AddSnippetFragment> {
-
   // Initial Selected Value
-  String dropdownvalue = 'C++';  
+  String dropdownvalue = 'C++';
 
   var dropdownItems = ["C++", "JavaScript", "Python"];
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              DropdownButton(
-                // Initial Value
-                value: dropdownvalue,
-                items: dropdownItems.map((String items) {
-                  return DropdownMenuItem(
-                    value: items,
-                    child: Text(items),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                setState(() {
-                  dropdownvalue = newValue!;
-                });
-              },
-              )
-            ]
-          )
-        )
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      DropdownButton(
+        // Initial Value
+        value: dropdownvalue,
+        items: dropdownItems.map((String items) {
+          return DropdownMenuItem(
+            value: items,
+            child: Text(items),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownvalue = newValue!;
+          });
+        },
       )
-    );
+    ]);
   }
 }
