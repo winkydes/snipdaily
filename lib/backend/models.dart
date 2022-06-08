c0ed86a42fa68fbeedcbf63d3334c9bfdd99d8b0
winkydes <keithlam0110@gmail.com>
2022-06-08 16:19:11 +0800

    explore fragment now shows data from database

--- lib/backend/models.dart
+++ lib/backend/models.dart
@@ -1,25 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Snippet {
-  final String id;
   final String code;
   final String title;
   final String description;
   final String language;
   final String formatType;
 
-  Snippet({required this. id, required this.code, required this.title, required this.description, required this.language, required this.formatType});
+  Snippet({ required this.code, required this.title, required this.description, required this.language, required this.formatType});
 
-  factory Snippet.fromFirestore(QuerySnapshot doc) {
-    Map data = doc as Map;
+  factory Snippet.fromSnapshot(DocumentSnapshot snapshot) {
     
     return Snippet(
-      id: data['id'] ?? '',
-      code: data['code'] ?? '',
-      title: data['title'] ?? '',
-      description: data['description'] ?? '',
-      language: data['language'] ?? '',
-      formatType: data['formatType'] ?? '',
+      code: snapshot['code'] ?? '',
+      title: snapshot['title'] ?? '',
+      description: snapshot['description'] ?? '',
+      language: snapshot['language'] ?? '',
+      formatType: snapshot['formatType'] ?? '',
     );
   }
 }
--- lib/fragments/ExploreFragment.dart
+++ lib/fragments/ExploreFragment.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:snipdaily/widgets/SnippetCardView.dart';
+
+import '../backend/models.dart';
 
 class ExploreFragment extends StatefulWidget {
   const ExploreFragment({Key? key}) : super(key: key);
@@ -8,17 +11,51 @@ class ExploreFragment extends StatefulWidget {
   State<ExploreFragment> createState() => _ExploreFragmentState();
 }
 class _ExploreFragmentState extends State<ExploreFragment> {
+
+  late final Stream<Iterable<Snippet>> _snippetStream = FirebaseFirestore.instance.collection('snippets').snapshots().map((item) => item.docs.map((doc) => Snippet.fromSnapshot(doc)));
+
+  late final List<Widget> snippetList = [];
+
+  @override
+  void initState() {
+    _snippetStream.forEach((element) => { 
+      element.forEach((snip) => snippetList.add(SnippetCardView(title: snip.title, language: snip.language, author: "keith")))
+     });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-        padding:
-            const EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 30),
-        children: const <Widget> [
-          SnippetCardView(
-              title: "",
-              language: "Test Language",
-              author: "Keith"
-          ),
-        ]);
+
+    return StreamBuilder<Iterable<Snippet>>(
+      stream: _snippetStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView(
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 30),
+          children: snippetList
+        );
+      }
+    );
+
+    // return ListView(
+    //     padding:
+    //         const EdgeInsets.only(left: 30, right: 30, top: 30, bottom: 30),
+    //     children: const <Widget> [
+    //       SnippetCardView(
+    //           title: "",
+    //           language: "Test Language",
+    //           author: "Keith"
+    //       ),
+    //     ]);
   }
 }
