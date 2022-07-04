be1ed861aa8f19961e58d42d1184f65b9a90b906
winkydes <keithlam0110@gmail.com>
2022-07-04 15:31:00 +0800

    add snippet suggestions everyday

---
+++ lib/fragments/TodaySnipSplashFragment.dart
@@ -0,0 +1,112 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../backend/models.dart';
+import '../main.dart';
+import '../widgets/LanguageLabel.dart';
+import '../widgets/TypeLabel.dart';
+
+class TodaySnipSplashFragment extends StatefulWidget {
+  const TodaySnipSplashFragment({Key? key}) : super(key: key);
+
+  @override
+  State<TodaySnipSplashFragment> createState() => _TodaySnipSplashFragmentState();
+}
+
+class _TodaySnipSplashFragmentState extends State<TodaySnipSplashFragment> {
+  int day = DateTime.now().day.toInt();
+
+  void updatePref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("login$day", false);
+  }
+
+  double customFontSize = 15;
+
+  resize(bool larger) {
+    if (larger && customFontSize < 30) {
+      setState(() {
+        customFontSize = customFontSize + 5;
+      });
+    }
+    else if (!larger && customFontSize > 10) {
+      setState(() {
+        customFontSize = customFontSize - 5;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Iterable<Snippet>>(
+      stream: FirebaseFirestore.instance
+        .collection('snippets')
+        .snapshots()
+        .map((item) => item.docs.map((doc) => Snippet.fromSnapshot(doc))),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        List<Snippet> randomSnippetList = snapshot.data!.toList();
+        randomSnippetList.shuffle();
+        Snippet randomSnip = randomSnippetList.first;
+        return Scaffold(
+          appBar: AppBar(title: const Text("Today's Snippet")),
+          body: ListView(
+            padding: const EdgeInsets.only(
+                left: 20, right: 20, top: 20, bottom: 20),
+            children: [
+              Text(
+                randomSnip.title,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    LanguageLabel(language: randomSnip.language),
+                    TypeLabel(type: randomSnip.type),
+                  ],
+                ),
+              ]),
+              Card(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 10, bottom: 20),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  child: Text(randomSnip.code,
+                      style: TextStyle(
+                        fontSize: customFontSize,
+                        fontFamily: 'Consolas',
+                      ))),
+              ),
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    randomSnip.description,
+                    style: const TextStyle(fontSize: 18.0, height: 1.5),
+                  )),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  updatePref();
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginWidget()));
+                },
+                child: const Text("Continue")),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:snipdaily/HomeScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:snipdaily/assets/GlobalTheme.dart';
@@ -9,6 +10,7 @@ import 'package:flutterfire_ui/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'fragments/AddTopicFragment.dart';
 import 'fragments/SettingsFragment.dart';
+import 'fragments/TodaySnipSplashFragment.dart';
 import 'fragments/adminFragments/AdminHomeFragment.dart';
 
 
@@ -43,6 +45,7 @@ class Login extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': ((context) => const LoginWidget()),
+        '/home': ((context) => const HomeScreen()),
         '/settings': ((context) => const SettingsFragment()),
         '/adminHome': ((context) => const AdminHomeFragment()),
         '/addTopic': ((context) => const AddTopicFragment()),
@@ -64,6 +67,8 @@ class _LoginWidgetState extends State<LoginWidget> {
   String userId = '';
   String? displayName = '';
   var db = FirebaseFirestore.instance;
+  int day = DateTime.now().day.toInt();
+  bool todayFirstLogin = false;
 
   Future<void> setAuthData(Map<String, dynamic> data) async {
     if (FirebaseAuth.instance.currentUser != null) {
@@ -72,6 +77,20 @@ class _LoginWidgetState extends State<LoginWidget> {
     }
   }
 
+  void loadTodayLogined() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.clear();
+    // print(prefs.getBool("login$day"));
+    setState(() {
+      todayFirstLogin = prefs.getBool("login$day") ?? true;
+    });
+  }
+
+  @override
+  void initState() {
+    loadTodayLogined();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
@@ -88,7 +107,6 @@ class _LoginWidgetState extends State<LoginWidget> {
               displayName = user.displayName;
             }
           });
-
           if (loginState == true) {
             final userBody = <String, dynamic> {
               "uid": userId,
@@ -106,9 +124,13 @@ class _LoginWidgetState extends State<LoginWidget> {
             );
           }
           // this email can later be change to any admin email
-          else if (snapshot.data?.email == "keithlam0110@gmail.com") {
+          if (snapshot.data?.email == "keithlam0110@gmail.com") {
             return const AdminHomeFragment();
           }
+          if (todayFirstLogin) {
+            return const TodaySnipSplashFragment();
+          }
+          
           return const HomeScreen();
         },
       );
