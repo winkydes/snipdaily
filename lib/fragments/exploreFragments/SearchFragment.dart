dc5ab11a9a21d421e852c74de6e7c947b0ef55d6
winkydes <keithlam0110@gmail.com>
2022-07-20 09:43:32 +0800

    search screen exception handling bug fix

--- lib/fragments/exploreFragments/SearchFragment.dart
+++ lib/fragments/exploreFragments/SearchFragment.dart
@@ -53,63 +53,68 @@ class _SearchFragmentState extends State<SearchFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<Iterable<Snippet>>(
-      stream: _snippetStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Center(
-            child: Text('Something went wrong'),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (!snapshot.hasData) {
-          return const Center(
-            child: Text(
-              'There is no snippets related to this topic yet, please wait for an update to the database.',
-              textAlign: TextAlign.center,
-            ),
-          );
-        }
-        return Scaffold(
-          appBar: AppBar(
-              // The search area here
-            title: SizedBox(
-              width: double.infinity,
-              height: 40,
-              child: Center(
-                child: TextField(
-                  controller: searchController,
-                  onChanged: (value) {onSearchTextChanged(value);},
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 5),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {
-                          /* Clear the search field */
-                          searchController.clear();
-                          onSearchTextChanged('');
-                        },
-                      ),
-                      hintText: 'Search...',
-                      fillColor: Colors.white,
-                      border: InputBorder.none),
+    return Scaffold(
+      appBar: AppBar(
+        // The search area here
+        title: SizedBox(
+          width: double.infinity,
+          height: 40,
+          child: Center(
+            child: TextField(
+              controller: searchController,
+              onChanged: (value) {onSearchTextChanged(value);},
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 5),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    /* Clear the search field */
+                    searchController.clear();
+                    onSearchTextChanged('');
+                  },
                 ),
-              ),
+                hintText: 'Search...',
+                fillColor: Colors.white,
+                border: InputBorder.none),
             ),
           ),
-          body: ListView.builder(
+        ),
+      ),
+      body: StreamBuilder<Iterable<Snippet>>(
+        stream: _snippetStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text(
+                'There are no snippets related to this topic yet, please wait for an update to the database.',
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          if (searchResult.isEmpty && searchController.text.isNotEmpty) {
+            return const Center (
+              child: Text('There are no snippets related to your search, please try again.'),
+            );
+          }
+          return ListView.builder(
             padding: const EdgeInsets.all(20),
             itemCount: searchResult.length,
             itemBuilder: (context, index) {
               return SnippetCardView(cardSnippet: searchResult[index],);
             }
-          ),
-        );
-      }
+          );
+        }
+      ),
     );
   }
 }
